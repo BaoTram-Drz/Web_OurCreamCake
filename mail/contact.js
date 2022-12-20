7dac4da0134de7df664b46544be97f359a830afa
BaoTram-Drz <90879088+BaoTram-Drz@users.noreply.github.com>
2022-12-20 21:43:52 +0700

    add-product-item

--- mail/contact.js
+++ mail/contact.js
@@ -38,7 +38,7 @@ $(function () {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                             .append("</button>");
-                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that our mail server is not responding. Please try again later!"));
+                    $('#success > .alert-danger').append($("<strong>").text("Xin lỗi " + name + ", có vẻ email của bạn bị sai. Hãy thử nhập lại nhé!"));
                     $('#success > .alert-danger').append('</div>');
                     $('#contactForm').trigger("reset");
                 },
